49bac5bd303b5731cc61167096dce3005698be44
Pete Stewart <petestew@gmail.com>
2020-08-24 20:04:13 -0500

    new board object being created

--- src/components/BoardForm/BoardForm.js
+++ src/components/BoardForm/BoardForm.js
@@ -1,9 +1,59 @@
 import React from 'react';
 
+import authData from '../../helpers/data/authData';
+
 class BoardForm extends React.Component {
+  state = {
+    name: '',
+    description: '',
+  }
+
+  changeNameEvent = (e) => {
+    e.preventDefault();
+    this.setState({ name: e.target.value });
+  };
+
+  descriptionEvent = (e) => {
+    e.preventDefault();
+    this.setState({ description: e.target.value });
+  };
+
+  saveBoardEvent = (e) => {
+    e.preventDefault();
+    const { name, description } = this.state;
+    const newBoard = {
+      name,
+      description,
+      uid: authData.getUid(),
+    };
+    console.warn(newBoard);
+  };
+
   render() {
     return (
-      <div className="BoardForm">BOARDFORM</div>
+      <form className = "col-6 offset-3">
+        <div className="form-group">
+          <label htmlFor="boardName">Board Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="boardName"
+            placeholder="Enter Board Name"
+            onChange={this.changeNameEvent}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="boardDescription">Board Description</label>
+          <input
+            type="text"
+            className="form-control"
+            id="boardDescription"
+            placeholder="Enter Board Name"
+            onChange={this.descriptionEvent}
+          />
+        </div>
+        <button className="btn btn-primary" onClick={this.saveBoardEvent}>Save Board</button>
+      </form>
     );
   }
 }
